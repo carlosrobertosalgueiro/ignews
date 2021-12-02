a5e782b1ba29b31baa5140d7f287a6ded3974fa8
carlos roberto <carlosrobertounopar01@hotmail.com>
2021-12-02 18:35:28 -0300

    Confirgurando titulo dinamicoa

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,16 @@
-
+import Head from 'next/head'
 import home from '../styles/home.module.scss'
 
 export default function Home() {
   return (
+    <>
+
+    <Head>
+    <title>Inicio | Ig.News</title>
+    </Head>
+
     <h1 className={home.title} >Hello Word</h1>
+
+    </>
   )
 }
