62688e52ba2cf68bbfb3199385edaea317227f98
carlos roberto <carlosrobertounopar01@hotmail.com>
2021-12-08 11:39:31 -0300

    Página home

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,27 @@
 import Head from 'next/head'
+import style from './home.module.scss'
 
 export default function Home() {
   return (
     <>
 
     <Head>
-    <title>Inicio | Ig.News</title>
+    <title>Home | Ig.News</title>
     </Head>
 
+    <main className={style.contentContainer} >
+        <section className={style.hero}>
+          <span> Hey, Welcone</span>
+          <h1> News About the <span>React</span> world</h1>
+          <p>
+              Get acess to all publication <br/>
+              <span>for $9.90 month</span>
+          </p>
+        </section>
 
+        <img src="/images/avatar.svg" alt="" />
+
+    </main>
 
     </>
   )
