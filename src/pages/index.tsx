23280427a0306e144b80b19e9b88c6407e48c0c9
carlos roberto <carlosrobertounopar01@hotmail.com>
2021-12-12 23:59:36 -0300

    SubscribeButton Concluido

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,7 @@
 import Head from 'next/head'
+import React from 'react'
 import style from './home.module.scss'
+import {SubscribeButton} from '../components/SubscribeButton'
 
 export default function Home() {
   return (
@@ -10,6 +12,7 @@ export default function Home() {
     </Head>
 
     <main className={style.contentContainer} >
+      
         <section className={style.hero}>
           <span> Hey, Welcone</span>
           <h1> News About the <span>React</span> world</h1>
@@ -17,7 +20,9 @@ export default function Home() {
               Get acess to all publication <br/>
               <span>for $9.90 month</span>
           </p>
+          <SubscribeButton/>
         </section>
+
 
         <img src="/images/avatar.svg" alt="" />
 
